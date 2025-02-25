4f7bfba074a88105a76f01454354b3009edcd2ae
Andrii Bzhestovskyi <andrii_bzhestovskyi@epam.com>
2025-02-25 14:09:51 +0200

    change gitlab-ci.yml

--- src/test/java/ui/BaseTests.java
+++ src/test/java/ui/BaseTests.java
@@ -2,7 +2,6 @@ package ui;
 
 import com.bookStore.tests.UiConfig;
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.WebDriverRunner;
 import com.github.javafaker.Faker;
 import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.BeforeAll;
@@ -16,7 +15,7 @@ public class BaseTests {
 
     @BeforeAll
     public static void before(){
-        Configuration.remote = "http://172.17.0.5:4444/wd/hub";
+        //Configuration.remote = "http://172.17.0.5/wd/hub";
         Configuration.browser = "chrome";
         Configuration.baseUrl = "http://172.17.0.5";
     }
